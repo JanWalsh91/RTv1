c43e5fd2c3962c8eb73aee7e103d87f018c1c16f
Jan WALSH <jwalsh@e3r7p4.42.fr>
2017-02-04 12:46:40 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/27 15:57:15 by jwalsh            #+#    #+#             */
-/*   Updated: 2017/02/03 17:29:47 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/02/04 12:41:39 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,14 @@ int	main(int ac, char **av)
 		printf("BIG ERROR");
 		return (0);
 	}
-	SDL_Window *win = SDL_CreateWindow("TEST", 100, 100, 640, 640, SDL_WINDOW_SHOWN);
+	SDL_Window *win = SDL_CreateWindow("TEST", 100, 100, scenes->res.x, scenes->res.y, SDL_WINDOW_SHOWN);
 	if (!win)
 	{
 		printf("SDL window ceartion error");
 		SDL_Quit();
 		return (0);
 	}
-	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 	if (!ren)
 	{
 		printf("SDL window ceartion error");
@@ -46,39 +46,40 @@ int	main(int ac, char **av)
 		SDL_Quit();
 		return (1);
 	}
-	SDL_SetRenderDrawColor(ren, 0xff, 0, 0, 0);
+
+	SDL_SetRenderDrawColor(ren, 0xff, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(ren);
 	int	y = 0;
-	while (y < 640)
+	while (y < scenes->res.y)
 	{
 		int x = 0;
-		while (x < 640)
+		while (x < scenes->res.x)
 		{
 			SDL_RenderDrawPoint(ren, y, x);
 			++x;
 		}
 		++y;
 	}
+	SDL_RenderPresent(ren);	
 	SDL_Event e;
+	(void)e;
 	int quit = 0;
 	while (!quit)
 	{
-		while (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_QUIT){
-				quit = 1;
-			}
-			if (e.type == SDL_KEYDOWN){
-				quit = 1;
-			}
-			if (e.type == SDL_MOUSEBUTTONDOWN){
-				quit = 1;
-			}
+		SDL_WaitEvent(&e);
+		if (e.type == SDL_WINDOWEVENT_CLOSE){
+			quit = 1;
 		}
-			SDL_RenderClear(ren);
-			SDL_RenderPresent(ren);
+	// 	if (e.type == SDL_KEYDOWN){
+	// 		quit = 1;
+	// 	}
+	// 	if (e.type == SDL_MOUSEBUTTONDOWN){
+	// 		quit = 1;
+	// 	}
 	}
-
-
+	
+	SDL_DestroyWindow(win);
+	SDL_Quit();
 	//send info to raytracer(scenes)
 	return (0);
 }
